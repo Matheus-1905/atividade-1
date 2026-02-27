b7b441a42241abea1a8c592cd8916c2bc9e52bea
Matheus Moraes <theus190509@gmail.com>
2026-02-26 21:07:39 -0300

    geração de um código teste

---
+++ teste.js
@@ -0,0 +1,7 @@
+let nome = "matheus"
+let telefone = 1140028022
+let email = "matheus1905@gmail.com"
+
+console.log(`me chamo ${nome}`)
+console.log(`meu numero é ${telefone}`)
+console.log(`meu meio de contato é ${email}`)
